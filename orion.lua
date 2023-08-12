e890048e17ecd8dec50f17bdef76e8f95a54b46c
marco <marcobreda34@gmail.com>
2023-08-12 12:30:02 -0600

    color update

--- orion.lua
+++ orion.lua
@@ -14,12 +14,12 @@ local OrionLib = {
 	Flags = {},
 	Themes = {
 		Default = {
-			Main = Color3.fromRGB(230, 163, 18),
+			Main = Color3.fromRGB(25, 25, 25),
 			Second = Color3.fromRGB(230, 163, 18),
-			Stroke = Color3.fromRGB(230, 163, 18),
-			Divider = Color3.fromRGB(230, 163, 18),
+			Stroke = Color3.fromRGB(60, 60, 60),
+			Divider = Color3.fromRGB(60, 60, 60),
 			Text = Color3.fromRGB(240, 240, 240),
-			TextDark = Color3.fromRGB(230, 163, 18)
+			TextDark = Color3.fromRGB(150, 150, 150)
 		}
 	},
 	SelectedTheme = "Default",
